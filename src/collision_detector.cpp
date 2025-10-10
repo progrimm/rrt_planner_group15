ddf44a6b275f3fae5536afc027d010c54e0d68bc
Aamund <aamgrim@gmail.com>
2025-10-10 18:00:04 +0200

    fix collision detection

--- src/collision_detector.cpp
+++ src/collision_detector.cpp
@@ -19,7 +19,10 @@ namespace rrt_planner {
         if (!costmap_->worldToMap(world_pos[0], world_pos[1], mx, my)) {
             return false; // outside the map
         }
-        return (costmap_->getCost(mx, my) == 0);
+        unsigned char cost = costmap_->getCost(mx, my);
+        // Consider free if cost is less than inscribed inflated obstacle (253)
+        // This allows navigation through cells with some cost but not lethal obstacles
+        return (cost < costmap_2d::INSCRIBED_INFLATED_OBSTACLE);
 
     }
 
